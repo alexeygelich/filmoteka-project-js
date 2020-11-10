05463b81407f6f4b0078a002776c11290f84a3e6
Alexander Tsurka <68517505+llex1@users.noreply.github.com>
2020-11-10 12:41:53 +0200

    pagination work

--- src/index.js
+++ src/index.js
@@ -2,59 +2,54 @@ import './sass/main.scss';
 
 import getMovies from './js/fetch.js';
 
-getMovies().then(data => console.log(data))
-console.log(getMovies());
 
 
-// const dom = {
-//   buttons: document.querySelector('.buttons'),
-//   span: document.querySelector('span'),
-//   outField: document.querySelector('#out-field')
-// }
-// let screen = '';
-// window.onload = function(event) {
-//   console.log(event.currentTarget.innerWidth);
-//   if (event.currentTarget.innerWidth > 1000) {
-//     screen = 'desktop';
-//     const draftArray = getMovies(1, screen);
-//     console.log(draftArray);
-//   }
-//   if (event.currentTarget.innerWidth > 700 && event.currentTarget.innerWidth < 1000) {
-//     screen = 'tablet';
-//     const draftArray = getMovies(1, screen);
-//     console.log(draftArray);
-//   }
-//   if (event.currentTarget.innerWidth < 700) {
-//     screen = 'mobile';
-//     const draftArray = getMovies(1, screen);
-//     console.log(draftArray);
-//   }
-// }
+const dom = {
+  buttons: document.querySelector('.buttons'),
+  span: document.querySelector('span'),
+  outField: document.querySelector('#out-field')
+}
+let screen = '';
+window.onload = function(event) {
+  console.log(event.currentTarget.innerWidth);
+  if (event.currentTarget.innerWidth > 1000) {
+    screen = 'desktop';
+    console.log(getMovies(1, screen));
+    getMovies(1, screen).then(data => console.log(data))
+  }
+  if (event.currentTarget.innerWidth > 700 && event.currentTarget.innerWidth < 1000) {
+    screen = 'tablet';
+    getMovies(1, screen).then(data => console.log(data))
+  }
+  if (event.currentTarget.innerWidth < 700) {
+    screen = 'mobile';
+    console.log(getMovies(1, screen));
+    getMovies(1, screen).then(data => console.log(data))
+  }
+}
 
-// dom.buttons.addEventListener('click', changePage)
+dom.buttons.addEventListener('click', changePage)
 
-// let currentPageOnUser = 1;
-// function changePage(event) {
-//   if(event.view.innerWidth> 1000){
-//     screen = 'desktop';
-//   } else if (event.view.innerWidth > 700 && event.view.innerWidth< 1000) {
-//     screen = 'tablet';
-//   } else {
-//     screen = 'mobile';
-//   }
-//   if (event.target.dataset.name == 'right') {
-//     currentPageOnUser+=1
-//     dom.span.innerText = currentPageOnUser;
-//     console.log(currentPageOnUser, screen);
-//     const draftArray = getMovies(currentPageOnUser, screen);
-//     console.log(draftArray);
-//   }
-//   if (event.target.dataset.name == 'left') {
-//     currentPageOnUser-=1
-//     dom.span.innerText = currentPageOnUser;
-//     console.log(currentPageOnUser, screen);
-//     const draftArray = getMovies(currentPageOnUser, screen);
-//     console.log(draftArray);
-//   }
-// }
+let currentPageOnUser = 1;
+function changePage(event) {
+  if(event.view.innerWidth> 1000){
+    screen = 'desktop';
+  } else if (event.view.innerWidth > 700 && event.view.innerWidth< 1000) {
+    screen = 'tablet';
+  } else {
+    screen = 'mobile';
+  }
+  if (event.target.dataset.name == 'right') {
+    currentPageOnUser+=1
+    dom.span.innerText = currentPageOnUser;
+    console.log(currentPageOnUser, screen);
+    getMovies(currentPageOnUser, screen).then(data => console.log(data))
+  }
+  if (event.target.dataset.name == 'left') {
+    currentPageOnUser-=1
+    dom.span.innerText = currentPageOnUser;
+    console.log(currentPageOnUser, screen);
+    getMovies(currentPageOnUser, screen).then(data => console.log(data))
+  }
+}
 
