94e01eb5cb9f016456a1750c9085acd138c4a9c7
Alexander Tsurka <68517505+llex1@users.noreply.github.com>
2020-11-10 03:01:07 +0200

    fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
 import './sass/main.scss';
+
+import getMovies from './js/fetch.js';
+
+const dom = {
+  buttons: document.querySelector('.buttons'),
+  span: document.querySelector('span'),
+  outField: document.querySelector('#out-field')
+}
+const fgdsfg = '';
+window.onload = function(event) {
+  console.log(event.currentTarget.innerWidth);
+  if (event.currentTarget.innerWidth > 1000) {
+    const draftArray = getMovies(1, 'desktop');
+    console.log(draftArray);
+  }
+  if (event.currentTarget.innerWidth > 700 && event.currentTarget.innerWidth < 1000) {
+    const draftArray = getMovies(1, 'tablet');
+    console.log(draftArray);
+  }
+  if (event.currentTarget.innerWidth < 700) {
+    
+      // const draftArray = async function() {
+      //   const arr = await getMovies(1, 'mobile').then(data => console.dir(data))
+        
+      // }
+      
+    const testt = getMovies(1, 'mobile')
+    console.log(testt);
+      
+      
+     
+         
+         
+            
+  
+    
+    
+  }
+}
+
+dom.buttons.addEventListener('click', changePage)
+
+function changePage(event) {
+ console.log(event);
+}
+
---
+++ src/js/fetch.js
@@ -0,0 +1,93 @@
+import refs from './refs.js';
+
+export default function(page = 1, screen = 'desktop', searchValue) {
+
+  //максимальна кількість елементів на екрані 
+  let countOfElements = 0;
+  switch(screen) {
+    case 'mobile': 
+      countOfElements = 4;
+      break;
+    case 'tablet':
+      countOfElements = 8;
+      break;
+    case 'desktop':
+      countOfElements = 9;
+  }
+  const calculateURIparameters = function() {
+    const URIparameters = {
+      currentPageOnServer: 1,
+      ElementsOnCurrentPage: 0,
+      isElementsOnPageBefore: false,
+      ElementsOnPageBefore: 0,
+      firstIndexOfElements: 0,
+      lastIndexOfElements: 0,
+      };
+      function go(obj) {
+        if (page*countOfElements > 20) {
+          this.currentPageOnServer = Math.ceil(page*countOfElements/20);
+          this.ElementsOnCurrentPage = page*countOfElements%20;
+          if (this.ElementsOnCurrentPage>=countOfElements) {
+            this.firstIndexOfElements = this.ElementsOnCurrentPage-countOfElements;
+            this.lastIndexOfElements = this.ElementsOnCurrentPage;
+          } else {
+            this.isElementsOnPageBefore = true;
+            this.ElementsOnPageBefore = countOfElements-this.ElementsOnCurrentPage;
+            this.firstIndexOfElements = 20-this.ElementsOnPageBefore;
+            this.lastIndexOfElements = this.ElementsOnCurrentPage;
+          }
+        } else {
+          this.lastIndexOfElements = page*countOfElements;
+          this.lastIndexOfElements-countOfElements<0 ? this.firstIndexOfElements = 0 : this.firstIndexOfElements = this.lastIndexOfElements-countOfElements;
+        }
+        return obj
+      }
+    return go.call(URIparameters, URIparameters)
+  }
+
+  console.dir(calculateURIparameters());
+
+
+ 
+  const draftArray = [];
+  const QUERY = function(obj) {
+    if(!obj.isElementsOnPageBefore) {
+      fetch(`${refs.defaultURL}?api_key=${refs.API}&page=${obj.currentPageOnServer}`)
+        .then(data => data.json())
+        .then(json => {
+          draftArray.push(json.results.slice(obj.firstIndexOfElements, obj.lastIndexOfElements));
+        })
+        
+    }
+    const multipleFetch = function() {
+      const pageBefore = fetch(`${refs.defaultURL}?api_key=${refs.API}&page=${obj.currentPageOnServer-1}`)
+        .then(data => data.json())
+        .then(json => json.results.slice(obj.firstIndexOfElements));
+
+      const curentPage = fetch(`${refs.defaultURL}?api_key=${refs.API}&page=${obj.currentPageOnServer}`)
+        .then(data => data.json())
+        .then(json => json.results.slice(0, obj.lastIndexOfElements));
+    
+      return Promise.all([pageBefore, curentPage])
+    }
+    if(obj.isElementsOnPageBefore) {
+      multipleFetch()
+        .then(data => {
+          console.log(data);
+          data.forEach(el => draftArray.push(...el));
+          return draftArray
+        })
+        // .then(data => draftArray)
+    }
+
+  }
+  QUERY(calculateURIparameters())
+  return draftArray
+ 
+}
+
+
+
+
+
+
