46cc11af684f4ea998ec5f621647131bb5b5da8b
ArtemSlipchencko <68220186+ArtemSlipchencko@users.noreply.github.com>
2020-11-10 16:19:07 +0200

    поиск (#21)
    * поиск
    
    * update
    
    Co-authored-by: Aleksey Gelich <alexeygelich@gmail.com>

---
+++ src/js/searchPrepare.js
@@ -0,0 +1,22 @@
+import getMovies from './fetch.js';
+
+const error = document.querySelector('.search-error');
+
+const input = document.querySelector('input');
+const form = document.querySelector('form');
+
+const search = function(value) {
+
+    if (value === "") {
+        alert("Введите запрос!")
+    } else {
+        getMovies(1, value);
+        error.innerHTML = "";
+    }
+
+};
+
+form.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    search(input.value);
+});
