3f7df0d1e615a5336c0bad687f04096fe97c5264
Aleksey Gelich <alexeygelich@gmail.com>
2020-11-10 14:40:35 +0200

    update

--- src/js/arr-for-render.js
+++ src/js/arr-for-render.js
@@ -1,12 +1,11 @@
 const arrRender = function (arrFilmList) {
   let newArrList = [];
 
-  document.documentElement.clientWidth >= 320 &&
   document.documentElement.clientWidth < 767
     ? (newArrList = arrFilmList.slice(0, 4))
     : '';
   document.documentElement.clientWidth >= 768 &&
-  document.documentElement.clientWidth < 1023
+  document.documentElement.clientWidth < 1024
     ? (newArrList = arrFilmList.slice(0, 8))
     : '';
   document.documentElement.clientWidth >= 1024
