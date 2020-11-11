c09d5efc05c7ffa64507883f89a360eaef90f62c
Alexander Tsurka <68517505+llex1@users.noreply.github.com>
2020-11-11 22:18:16 +0200

    Paginator final (BIG fix) (#31)
    * remake paginator, add getMovieData + render
    
    * fix query function
    
    * little fix
    
    * very BIG fix
    
    Co-authored-by: alexeygelich <65009861+alexeygelich@users.noreply.github.com>

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,4 +1,9 @@
+
+//! ВИПРАВИТИ ШАПБЛОН (КНОПКА close)
+import modalTemplate from './templates/modalTemplate-new.hbs';
+
 import localStorage from "./localStorage";
+
 export default function (arr) {
   const filmList = document.querySelector(".film-list");
   let elId;
@@ -7,63 +12,25 @@ export default function (arr) {
       if (el.className === "film-list-item") {
         elId = +el.dataset.id;
 
+
+  const onModalOpen = e => {
+    e.path.forEach(el => {
+      if (el.className === 'film-list-item') {
+        const elId = +el.dataset.id;
         // arr.forEach(el => {
         // if (el.id === elId) {
-        document.body.classList.add("stop-scroll");
-        const filmModal = document.querySelector(".modal-section");
-        filmModal.innerHTML = `
-    <button class="close-modal-btn" type="button">X</button>
-      <img class="film-image" src="http://image.tmdb.org/t/p/w440_and_h660_face${arr[elId].poster_path}" alt="${arr[elId].title}" />
-      <div class="info-section">
-        <h2 class="title">${arr[elId].title}</h2>
-        <div class="modal-info">
-          <ul class="list-info">
-            <li>
-              <p class="about-film">Vote / Votes</p>
-            </li>
-            <li>
-              <p class="about-film">Popularity</p>
-            </li>
-            <li>
-              <p class="about-film">Original Title</p>
-            </li>
-            <li>
-              <p class="about-film genre">Genre</p>
-            </li>
-          </ul>
-          <ul class="list-info">
-            <li>
-              <p class="about-film-info">
-                <span class="about-film-vote">${arr[elId].vote_average}</span> / ${arr[elId].vote_count}
-              </p>
-            </li>
-            <li>
-              <p class="about-film-info">${arr[elId].popularity}</p>
-            </li>
-            <li>
-              <p class="about-film-info">${arr[elId].title}</p>
-            </li>
-            <li>
-              <p class="about-film-info">${arr[elId].genre_ids}</p>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <h3 class="subtitle">ABOUT</h3>
-          <p class="description">
-            ${arr[elId].overview}
-          </p>
-        </div>
-        <button class="first add-btn" type="button">ADD TO WATCHED</button>
-        <button class="second add-btn" type="button">ADD TO QUEUE</button>
-    </div>`;
-      }
-    });
+        const test = arr.find(el => el.id === elId);
+        document.body.classList.add('stop-scroll');
+        const filmModal = document.querySelector('.modal-section');
+        filmModal.innerHTML = modalTemplate(test);
+      } 
+    })
+        
+    const modalOn = document.querySelector('.backdrop');
+    modalOn.classList.remove('is-hidden');
 
-    localStorage(arr[elId]);
+    localStorage(test);
 
-    const modalOn = document.querySelector(".backdrop");
-    modalOn.classList.remove("is-hidden");
 
     const onEscapeClose = function (e) {
       console.log(e.key);
@@ -83,6 +50,7 @@ export default function (arr) {
         modalOn.classList.add("is-hidden");
       }
     });
+
 
     const closeBtn = document.querySelector(".close-modal-btn");
     closeBtn.addEventListener("click", (e) => {
--- src/js/pagination.js
+++ src/js/pagination.js
@@ -4,120 +4,129 @@ import Pagination from "tui-pagination"
 import "tui-pagination/dist/tui-pagination.css"
 import refs from './refs.js';
 import getFetch from './fetch.js';
-
 import getMoviesData from './getMoviesData.js';
 import renderMain from './render-film.js';
+import modalOpen from './modal.js';
 
 
-//! перший пагінатор генеруємо при рендері main.page
-//! потрібна перевірка на ширину вікна, і передаємо ці данні в опції пагінатора
-var pagination1 = new Pagination("pagination", {
-  totalItems: 10000,
-  itemsPerPage: 8,
-  visiblePages: 7,
-})
 
-const ref = {
-  pagination: document.querySelector('#pagination'),
-}
-ref.pagination.addEventListener('click', changePage);
-let page = 1;
-
-function changePage(event, searchValue=false) {
-  // console.log(event.target);
-//! не корректна робота погінатора, якщо клікаємо на стрілочку в середині спана
-//! потрібно додати data атрибут
-
-  const searchClassDisable = ([...arr]) => arr.includes('tui-is-disabled');
-  // console.log(searchClassDisable(event.target.classList));
-  if(!searchClassDisable(event.target.classList)){
-  const screen  = event.view.innerWidth;
+export default async function(value=false, promisTotalItems) {
+  const totalItems = await promisTotalItems;
+  let page = 1;
+  const screen  = document.documentElement.clientWidth;
+  let buttonsOnPage = 5;
   let countOfElements = 4;
-  if (screen > 768 && screen < 1200) {
-    countOfElements = 8
-  } else if (screen > 1200) {
-    countOfElements = 9
+  if (screen >= 768 && screen < 1024) {
+    countOfElements = 8;
+    buttonsOnPage = 7;
+  } else if (screen >= 1024) {
+    countOfElements = 9;
+    buttonsOnPage = 7;
   }
-  // const searchClassDisable = ([...arr]) => arr.includes('tui-is-disabled');
-  // if(!searchClassDisable(event.target.classList)){
-    if(event.target.textContent === "next") {
-      page+=1;
-    } else if(event.target.textContent === "prev"){
-      page-=1;
-    } else if(event.target.textContent === "..."){
-      let arrChildren = [...event.currentTarget.children];
-      let activeChildren = arrChildren.find(el => el.className == "tui-page-btn tui-is-selected");
-      page = Number(activeChildren.textContent);
-    } else if(event.target.textContent === "last"){
-      page = 10000/countOfElements;
-    } else if(event.target.textContent === "first"){
-      page = 1;
-    } 
-    else {
-      page = Number(event.target.textContent) || page;
-    }
-  // } 
-  const calculateURIparameters = function() {
-    const URIparameters = {
-      currentPageOnServer: 1,
-      ElementsOnCurrentPage: 0,
-      isElementsOnPageBefore: false,
-      ElementsOnPageBefore: 0,
-      firstIndexOfElements: 0,
-      lastIndexOfElements: 0,
-      };
-      function go(obj) {
-        if (page*countOfElements > 20) {
-          this.currentPageOnServer = Math.ceil(page*countOfElements/20); 
-          this.ElementsOnCurrentPage = page*countOfElements%20; 
-          if (this.ElementsOnCurrentPage>=countOfElements) {
-            this.firstIndexOfElements = this.ElementsOnCurrentPage-countOfElements; 
-            this.lastIndexOfElements = this.ElementsOnCurrentPage;
-          } else if (this.ElementsOnCurrentPage === 0){
-            this.firstIndexOfElements = 20-countOfElements;
-            this.lastIndexOfElements = 20;
+  //! перший пагінатор генеруємо при рендері main.page
+  //! потрібна перевірка на ширину вікна, і передаємо ці данні в опції пагінатора
+  var pagination1 = new Pagination("pagination", {
+    totalItems: totalItems,
+    itemsPerPage: countOfElements,
+    visiblePages: buttonsOnPage,
+  })
+  
+  const ref = {
+    pagination: document.querySelector('#pagination'),
+  }
+  ref.pagination.addEventListener('click', changePage);
+  function changePage(event) {
+  ref.pagination.removeEventListener('click', changePage);
+  //! не корректна робота погінатора, якщо клікаємо на стрілочку в середині спана
+  //! потрібно додати data атрибут
+    const searchClassDisable = ([...arr]) => arr.includes('tui-is-disabled');
+    // console.log(searchClassDisable(event.target.classList));
+    if(!searchClassDisable(event.target.classList)){
+    // const searchClassDisable = ([...arr]) => arr.includes('tui-is-disabled');
+    // if(!searchClassDisable(event.target.classList)){
+      if(event.target.textContent === "next") {
+        page+=1;
+      } else if(event.target.textContent === "prev"){
+        page-=1;
+      } else if(event.target.textContent === "..."){
+        let arrChildren = [...event.currentTarget.children];
+        let activeChildren = arrChildren.find(el => el.className == "tui-page-btn tui-is-selected");
+        page = Number(activeChildren.textContent);
+      } else if(event.target.textContent === "last"){
+        page = totalItems/countOfElements;
+      } else if(event.target.textContent === "first"){
+        page = 1;
+      } 
+      else {
+        page = Number(event.target.textContent) || page;
+      }
+    // } 
+    const calculateURIparameters = function() {
+      const URIparameters = {
+        currentPageOnServer: 1,
+        ElementsOnCurrentPage: 0,
+        isElementsOnPageBefore: false,
+        ElementsOnPageBefore: 0,
+        firstIndexOfElements: 0,
+        lastIndexOfElements: 0,
+        };
+        function go(obj) {
+          if (page*countOfElements > 20) {
+            this.currentPageOnServer = Math.ceil(page*countOfElements/20); 
+            this.ElementsOnCurrentPage = page*countOfElements%20; 
+            if (this.ElementsOnCurrentPage>=countOfElements) {
+              this.firstIndexOfElements = this.ElementsOnCurrentPage-countOfElements; 
+              this.lastIndexOfElements = this.ElementsOnCurrentPage;
+            } else if (this.ElementsOnCurrentPage === 0){
+              this.firstIndexOfElements = 20-countOfElements;
+              this.lastIndexOfElements = 20;
+            }
+              else {
+              this.isElementsOnPageBefore = true;
+              this.ElementsOnPageBefore = countOfElements-this.ElementsOnCurrentPage;
+              this.firstIndexOfElements = 20-this.ElementsOnPageBefore;
+              this.lastIndexOfElements = this.ElementsOnCurrentPage;
+            }
+          } else {
+            this.lastIndexOfElements = page*countOfElements;
+            this.lastIndexOfElements-countOfElements<0 ? this.firstIndexOfElements = 0 : this.firstIndexOfElements = this.lastIndexOfElements-countOfElements;
           }
-            else {
-            this.isElementsOnPageBefore = true;
-            this.ElementsOnPageBefore = countOfElements-this.ElementsOnCurrentPage;
-            this.firstIndexOfElements = 20-this.ElementsOnPageBefore;
-            this.lastIndexOfElements = this.ElementsOnCurrentPage;
-          }
-        } else {
-          this.lastIndexOfElements = page*countOfElements;
-          this.lastIndexOfElements-countOfElements<0 ? this.firstIndexOfElements = 0 : this.firstIndexOfElements = this.lastIndexOfElements-countOfElements;
+          return obj
         }
-        return obj
-      }
-    return go.call(URIparameters, URIparameters)
-  }
-  //---обчислений об'єкт з параметрами URI---
-  //console.dir(calculateURIparameters());
-    const QUERY = async function(obj) {
-      let draftArray = [];
-  //! елементи на 2(двох) сторінка на сервері --->  [FALSE]
-      if(!obj.isElementsOnPageBefore) {
-          await getMoviesData(getFetch(`${obj.currentPageOnServer}`))
-            .then(data => {
-            return draftArray = [...data.slice(obj.firstIndexOfElements, obj.lastIndexOfElements)]
-          })
-      }
-      const multipleFetch = async function() {
-        const pageBefore = await getMoviesData(getFetch(`${obj.currentPageOnServer-1}`))
-          .then(data => data.slice(obj.firstIndexOfElements))
-        const curentPage = await getMoviesData(getFetch(`${obj.currentPageOnServer}`))
-          .then(data => data.slice(0, obj.lastIndexOfElements))
-        return [pageBefore, curentPage]
-      }
-  //! елементи на 2(двох) сторінка на сервері --->  [TRUE]
-      if(obj.isElementsOnPageBefore) {
-         await multipleFetch()
-          .then(data => {
-            data.forEach(el => draftArray.push(...el));
-          })
-      }
-      renderMain(draftArray)
+      return go.call(URIparameters, URIparameters)
     }
-    QUERY(calculateURIparameters())
+    //---обчислений об'єкт з параметрами URI---
+    // console.dir(calculateURIparameters());
+      const QUERY = async function(obj) {
+        let draftArray = [];
+    //! елементи на 2(двох) сторінка на сервері --->  [FALSE]
+        if(!obj.isElementsOnPageBefore) {
+            await getMoviesData(getFetch(`${obj.currentPageOnServer}`, value))
+              .then(data => {
+              return draftArray = [...data.slice(obj.firstIndexOfElements, obj.lastIndexOfElements)]
+            })
+        }
+        const multipleFetch = async function() {
+          const pageBefore = await getMoviesData(getFetch(`${obj.currentPageOnServer-1}`, value))
+            .then(data => data.slice(obj.firstIndexOfElements))
+          const curentPage = await getMoviesData(getFetch(`${obj.currentPageOnServer}`, value))
+            .then(data => data.slice(0, obj.lastIndexOfElements))
+          return [pageBefore, curentPage]
+        }
+    //! елементи на 2(двох) сторінка на сервері --->  [TRUE]
+        if(obj.isElementsOnPageBefore) {
+           await multipleFetch()
+            .then(data => {
+              data.forEach(el => draftArray.push(...el));
+            })
+        }
+        renderMain(draftArray);
+        modalOpen(draftArray);
+      }
+      QUERY(calculateURIparameters())
+    }
+    ref.pagination.addEventListener('click', changePage);
   }
+
 }
+
