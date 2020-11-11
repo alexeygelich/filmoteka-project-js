35ec3bd3ae62de6a9fcbd1fa297f123cc0480f45
Alexander Tsurka <68517505+llex1@users.noreply.github.com>
2020-11-11 10:54:23 +0200

    Pagination (#24)
    * перенёс свои наработки
    
    * start remake pagination
    
    * pagination
    
    * pagination is completed (fix query)
    
    Co-authored-by: Ostap Pluzhnik <plostvik@gmail.com>
    Co-authored-by: alexeygelich <65009861+alexeygelich@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
-import "./sass/main.scss";
+import "./sass/main.scss"
+import "./js/searchPrepare.js"
+import "./js/pagination.js"
 import "./js/search.js";
--- src/js/searchPrepare.js
+++ src/js/searchPrepare.js
@@ -1,5 +1,6 @@
 import getMovies from "./fetch.js";
 
+
 const error = document.querySelector(".search-error");
 
 export const search = function (value) {
