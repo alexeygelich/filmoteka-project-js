e612581ae0a1cad15ccfc80c73806b3e6f86244a
Омельянов Дима <domelyanov@hotmail.com>
2020-11-11 16:24:38 +0200

    local Storage DONE

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,15 +1,16 @@
+import localStorage from "./localStorage";
 export default function (arr) {
-  const filmList = document.querySelector('.film-list');
+  const filmList = document.querySelector(".film-list");
+  let elId;
+  const onModalOpen = (e) => {
+    e.path.forEach((el) => {
+      if (el.className === "film-list-item") {
+        elId = +el.dataset.id;
 
-  const onModalOpen = e => {
-  
-    e.path.forEach(el => {
-      if (el.className === 'film-list-item') {
-        const elId = +el.dataset.id;
         // arr.forEach(el => {
         // if (el.id === elId) {
-        document.body.classList.add('stop-scroll');
-        const filmModal = document.querySelector('.modal-section');
+        document.body.classList.add("stop-scroll");
+        const filmModal = document.querySelector(".modal-section");
         filmModal.innerHTML = `
     <button class="close-modal-btn" type="button">X</button>
       <img class="film-image" src="http://image.tmdb.org/t/p/w440_and_h660_face${arr[elId].poster_path}" alt="${arr[elId].title}" />
@@ -55,47 +56,41 @@ export default function (arr) {
         </div>
         <button class="first add-btn" type="button">ADD TO WATCHED</button>
         <button class="second add-btn" type="button">ADD TO QUEUE</button>
-    </div>`
+    </div>`;
       }
-    })
-        
+    });
 
-    const modalOn = document.querySelector('.backdrop')
-    modalOn.classList.remove('is-hidden')
-      
-  
+    localStorage(arr[elId]);
+
+    const modalOn = document.querySelector(".backdrop");
+    modalOn.classList.remove("is-hidden");
 
     const onEscapeClose = function (e) {
       console.log(e.key);
       if (e.key === "Escape") {
-        modalOn.classList.add('is-hidden');
-        document.body.classList.remove('stop-scroll');
-        window.removeEventListener('keydown', onEscapeClose);
+        modalOn.classList.add("is-hidden");
+        document.body.classList.remove("stop-scroll");
+        window.removeEventListener("keydown", onEscapeClose);
       }
-    }
-      
-    window.addEventListener('keydown', onEscapeClose);
+    };
 
-    modalOn.addEventListener('click', e => {
+    window.addEventListener("keydown", onEscapeClose);
+
+    modalOn.addEventListener("click", (e) => {
       if (e.target === e.currentTarget) {
-        window.removeEventListener('keydown', onEscapeClose);
-        document.body.classList.remove('stop-scroll');
-        modalOn.classList.add('is-hidden');
+        window.removeEventListener("keydown", onEscapeClose);
+        document.body.classList.remove("stop-scroll");
+        modalOn.classList.add("is-hidden");
       }
-    }
-    )
+    });
 
-    const closeBtn = document.querySelector('.close-modal-btn');
-    closeBtn.addEventListener('click', (e) => {
-      window.removeEventListener('keydown', onEscapeClose);
-      document.body.classList.remove('stop-scroll');
-      modalOn.classList.add('is-hidden');
-    })
+    const closeBtn = document.querySelector(".close-modal-btn");
+    closeBtn.addEventListener("click", (e) => {
+      window.removeEventListener("keydown", onEscapeClose);
+      document.body.classList.remove("stop-scroll");
+      modalOn.classList.add("is-hidden");
+    });
+  };
 
-
-    
-  }
-
-
-  filmList.addEventListener('click', onModalOpen)
+  filmList.addEventListener("click", onModalOpen);
 }
