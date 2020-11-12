39613b8e8227f45485b5a021c0e4e476699c00cf
Alexander Tsurka <68517505+llex1@users.noreply.github.com>
2020-11-12 19:06:45 +0200

    loader start (30%)

--- src/js/render-film.js
+++ src/js/render-film.js
@@ -2,12 +2,18 @@ import mainTemplate from "./templates/mainTemplate.hbs";
 import refs from './refs.js';
 
 const mainDiv = document.querySelector('.main');
+const loaderDiv = document.querySelector('.loader');
 
 export default function (arr) {
     mainDiv.innerHTML = `
     <ul class="film-list">
     </ul>
     `;
+    const ulFilmList = document.querySelector('.main ul.film-list');
+    setTimeout(()=> {
+        loaderDiv.classList.add('is-hidden');
+        ulFilmList.classList.add('show');
+    }, 800);
     const filmList = document.querySelector('.film-list');
 
     const film = mainTemplate(arr);
