3f8e12b421991f9e248659531e71a9337207abd4
Alexander Tsurka <68517505+llex1@users.noreply.github.com>
2020-11-13 10:13:34 +0200

    loader start (30%) (#41)
    Co-authored-by: alexeygelich <65009861+alexeygelich@users.noreply.github.com>

--- src/js/render-film.js
+++ src/js/render-film.js
@@ -1,8 +1,20 @@
 import mainTemplate from "./templates/mainTemplate.hbs";
 
-const filmList = document.querySelector('.film-list');
+
+const mainDiv = document.querySelector('.main');
+const loaderDiv = document.querySelector('.loader');
 
 export default function (arr) {
+    mainDiv.innerHTML = `
+    <ul class="film-list">
+    </ul>
+    `;
+    const ulFilmList = document.querySelector('.main ul.film-list');
+    setTimeout(()=> {
+        loaderDiv.classList.add('is-hidden');
+        ulFilmList.classList.add('show');
+    }, 800);
+    const filmList = document.querySelector('.film-list');
 
 
     const film = mainTemplate(arr);
