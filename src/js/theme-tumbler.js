53ff7994bee18d7ce398813303429c74d6d031a3
Alina Leonova <alinaa.leonova@gmail.com>
2020-11-14 23:22:15 +0200

    theme swithcer

--- src/js/theme-tumbler.js
+++ src/js/theme-tumbler.js
@@ -7,13 +7,22 @@ const changeTheme = document.querySelector('#theme-switch-toggle');
 
 changeTheme.addEventListener('change', savedChangeTheme);
 const themeShow = document.querySelector('body');
-console.log(themeShow);
+
+
+const darkFooter = document.querySelector('.footer');
+console.log(darkFooter);
+
+
 
 function defaultTheme() {
     if (localStorage.getItem('Theme') === Theme.DARK) {
         themeShow.classList.add(Theme.DARK);
+
+        darkFooter.classList.add('footer-dark')
+
         changeTheme.checked = true;
         themeShow.classList.remove(Theme.LIGHT);
+        darkFooter.classList.remove('footer-light')
     } else {
         localStorage.setItem('Theme', 'light-theme');
         themeShow.classList.add(Theme.LIGHT);
@@ -23,6 +32,8 @@ function defaultTheme() {
 function savedChangeTheme(e) {
     console.log(e);
     if (e.target.checked) {
+
+        console.log(e.target);
         themeShow.classList.add(Theme.DARK);
         themeShow.classList.remove(Theme.LIGHT);
         localStorage.setItem('Theme', 'dark-theme');
