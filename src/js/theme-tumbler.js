5ee099d8295710c584294b19c936fcff76144212
Alina Leonova <alinaa.leonova@gmail.com>
2020-11-15 21:53:34 +0200

    theme footer

--- src/js/theme-tumbler.js
+++ src/js/theme-tumbler.js
@@ -3,20 +3,18 @@ const Theme = {
     DARK: 'dark-theme',
 }
 
-
 const changeTheme = document.querySelector('#theme-switch-toggle');
 
 changeTheme.addEventListener('change', savedChangeTheme);
 const themeShow = document.querySelector('body');
 
 
-
 const darkFooter = document.querySelector('.footer');
 
 
-
-if (localStorage.getItem('Theme') === 'dark-theme') { 
-    themeShow.classList.add(Theme.DARK); 
+if (localStorage.getItem('Theme') === 'dark-theme') {
+    themeShow.classList.add(Theme.DARK);
+    darkFooter.classList.add('footer-dark');
     changeTheme.checked = true;
 }
 
@@ -24,8 +22,6 @@ if (localStorage.getItem('Theme') === 'dark-theme') {
 function defaultTheme() {
     if (localStorage.getItem('Theme') === Theme.DARK) {
         themeShow.classList.add(Theme.DARK);
-
-        darkFooter.classList.add('footer-dark')
 
         changeTheme.checked = true;
         themeShow.classList.remove(Theme.LIGHT);
@@ -37,16 +33,18 @@ function defaultTheme() {
 }
 
 function savedChangeTheme(e) {
-    console.log(e);
     if (e.target.checked) {
-
-        console.log(e.target);
         themeShow.classList.add(Theme.DARK);
+        darkFooter.classList.add('footer-dark');
+        darkFooter.classList.remove('footer-light')
+
         themeShow.classList.remove(Theme.LIGHT);
         localStorage.setItem('Theme', 'dark-theme');
     } else if (!e.target.checked) {
         themeShow.classList.add(Theme.LIGHT);
         themeShow.classList.remove(Theme.DARK);
+        darkFooter.classList.remove('footer-dark')
+        darkFooter.classList.add('footer-light')
         localStorage.setItem('Theme', 'light-theme');
     }
 }
